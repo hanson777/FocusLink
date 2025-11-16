a84180f015eb471677fee3911d2ce030f24fa022
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-11-15 23:45:58 -0800

    included users_router in app

--- backend/__init__.py
+++ backend/__init__.py
@@ -7,6 +7,7 @@ from backend.friends.routes import friends_router
 from backend.user_status.routes import user_status_router
 from backend.studysession.routes import studysession_router
 from fastapi.middleware.cors import CORSMiddleware
+from backend.users.routes import users_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting app")
@@ -29,5 +30,6 @@ app.include_router(friends_router, prefix="/friends", tags=["friends"])
 app.include_router(user_status_router, prefix="/user-status", tags=["user-status"])
 app.include_router(studysession_router, prefix="/study-sessions", tags=["study-sessions"])
 
+app.include_router(users_router, prefix="/users", tags=["users"])
 
 
